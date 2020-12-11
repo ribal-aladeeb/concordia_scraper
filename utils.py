78589a4b20ff0ac5f4c20a66e43016c0f214f680
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-12-11 05:43:13 -0500

    Load index from disk

--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ import json
 from tqdm import tqdm
 
 
-CACHE_DIR='cache/'
+CACHE_DIR = 'cache/'
+INDEX_FILE = 'inverted_index.txt'
 
 
 def ensure_dir_exists(dirname):
@@ -13,7 +14,7 @@ def ensure_dir_exists(dirname):
         os.makedirs(dirname)
 
 
-def save_index(index, output_file='inverted_index.txt'):
+def save_index(index, output_file=INDEX_FILE):
 
     if CACHE_DIR not in output_file:
         output_file = os.path.join(CACHE_DIR, output_file)
@@ -24,3 +25,27 @@ def save_index(index, output_file='inverted_index.txt'):
         for term_posting_pair in tqdm(term_sorted_index):
             print(json.dumps(term_posting_pair), file=f)
     return True
+
+
+def load_index(input_file=INDEX_FILE):
+
+    if CACHE_DIR not in input_file:
+        input_file = os.path.join(CACHE_DIR, input_file)
+
+    print(f'Loading index from {input_file}')
+    inverted_index = {}
+
+    with open(input_file, mode='r') as f:
+        for line in tqdm(f):
+            buffer: list = json.loads(line)
+            term = buffer[0]
+            inverted_index[term] = buffer[1]
+
+    return inverted_index
+
+
+if __name__ == '__main__':
+    index = load_index()
+    print(index['hanna'])
+    save_index(index,output_file='copycat.txt')
+
