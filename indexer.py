611ccf7fd67e0eb88957ca82959772aef1b8fe51
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-12-11 02:00:09 -0500

    Refactor stemming (move it to document loading)
    This is done in hopes of reducing the runtime of the of the indexer.

--- indexer.py
+++ indexer.py
@@ -10,8 +10,10 @@ TOKENIZING_REGEX = r"[a-zA-Z]+[-']{0,1}[a-zA-Z]*[']{0,1}"  # supports hyphenated
 utils.ensure_dir_exists(CACHE_DIR)
 
 
-def custom_tokenizer(text: str, tokenizer) -> list:
+def custom_tokenizer(text: str, tokenizer, stem_doc=False) -> list:
     tokens = tokenizer.tokenize(text)
+    stemmer = nltk.stem.PorterStemmer()
+
     cleaned_tokens = []
 
     for token in tokens:
@@ -20,12 +22,15 @@ def custom_tokenizer(text: str, tokenizer) -> list:
         while token[-1] == "-" or token[-1] == "'":
             token = token[:-1]
         if 1 < len(token):
+            token = stemmer.stem(token)
             cleaned_tokens.append(token)
 
     return cleaned_tokens
 
 
-def load_and_tokenize_documents(documents_folder='documents/', regex=TOKENIZING_REGEX) -> dict:
+def load_and_tokenize_documents(documents_folder='documents/',
+                                regex=TOKENIZING_REGEX,
+                                stem_docs=False) -> dict:
     '''
     Traverses all directories in documents_folder, builds a mapping of docIDs ->
     document tokens. As a side effect, this function will calculate each
@@ -45,7 +50,8 @@ def load_and_tokenize_documents(documents_folder='documents/', regex=TOKENIZING_
         except ValueError as e:
             print(e)
             print(f'{documents_folder} should only contain integer directory names.')
-            print(f'Please do not manual write anything into this directory. It should be entirely managed by the program')
+            print(f'Please do not manually write anything into this directory.')
+            print(f'It should be entirely managed by the program.')
             continue  # skip this faulty directory name
 
         document_path = os.path.join(documents_folder, docID_dir)
@@ -53,7 +59,7 @@ def load_and_tokenize_documents(documents_folder='documents/', regex=TOKENIZING_
         with open(os.path.join(document_path, 'text'), mode='r') as f:
             doc_text = f.read()
 
-        docs[docID] = custom_tokenizer(doc_text, tokenizer)
+        docs[docID] = custom_tokenizer(doc_text, tokenizer, stem_doc=stem_docs)
 
         with open(os.path.join(document_path, 'length'), mode='w')as f:
             f.write(str(len(docs[docID])))
@@ -61,20 +67,17 @@ def load_and_tokenize_documents(documents_folder='documents/', regex=TOKENIZING_
     return docs
 
 
-def create_index(docs: dict, stem_index=True) -> dict:
+def create_index(docs: dict, ) -> dict:
     '''
     Creates the inverted index in the following format:
     {term: [df, [(docID, tf), (docID, tf), ...]]}
     '''
 
-    stemmer = nltk.stem.PorterStemmer()
     index = {}
     print('Building the index')
 
     for docID in tqdm(docs):
         tokens = docs[docID]
-        if stem_index:
-            tokens = [stemmer.stem(t) for t in tokens]
 
         term_frequencies = dict(Counter(tokens))
         for term in term_frequencies:
@@ -103,8 +106,8 @@ def main():
             print('Please enter a correct response.')
 
     should_stem = True if should_stem in ['y', 'yes'] else False
-    docs = load_and_tokenize_documents()
-    index = create_index(docs, stem_index=should_stem)
+    docs = load_and_tokenize_documents(stem_docs=should_stem)
+    index = create_index(docs)
     utils.save_index(index)
     print(f'Index contains {len(index)} unique terms.')
 
